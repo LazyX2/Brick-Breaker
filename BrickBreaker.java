3843a9e6a11bd7a53e893adedd52fbb3bc2917ae
Shy <94857850+LazyX2@users.noreply.github.com>
2022-10-21 00:26:01 +0300

    Update BrickBreaker.java

--- BrickBreaker.java
+++ BrickBreaker.java
@@ -13,17 +13,17 @@ public class BrickBreaker extends JFrame {
     public List<Brick> bricks = new ArrayList<>();
     public int[] redBall = new int[] {
             400, 500,
-            1, 3
+            3, 3
     };
     public int[] greenBall = new int[] {
             500, 400,
-            1, 3
+            3, 3
     };
     public int[] blueBall = new int[] {
             300, 300,
-            1, 3
+            3, 3
     };
-    public int plane = 10;
+    public int plane = 10, score = 0;
 
     class Brick {
 
@@ -39,11 +39,6 @@ public class BrickBreaker extends JFrame {
 
     public boolean active = true, pause = true;
 
-    public double generateChanger() {
-        double c = 0.1*Math.random()+1;
-        return -c;
-    }
-
     class Screen extends Canvas {
 
         public Screen() {
@@ -56,36 +51,33 @@ public class BrickBreaker extends JFrame {
 
             if (active) {
                 if (redBall[1] <= 0) {
-                    redBall[3] *= generateChanger();
-                    redBall[2] *= generateChanger();
+                    redBall[3] *= -1;
                 }
                 if (greenBall[1] <= 0) {
-                    greenBall[3] *= generateChanger();
-                    greenBall[2] *= generateChanger();
+                    greenBall[3] *= -1;
                 }
                 if (blueBall[1] <= 0) {
-                    blueBall[3] *= generateChanger();
-                    blueBall[2] *= generateChanger();
+                    blueBall[3] *= -1;
                 }
 
                 if (redBall[0] <= 0) {
-                    redBall[2] *= generateChanger();
+                    redBall[2] *= -1;
                 }
                 if (greenBall[0] <= 0) {
-                    greenBall[2] *= generateChanger();
+                    greenBall[2] *= -1;
                 }
                 if (blueBall[0] <= 0) {
-                    blueBall[2] *= generateChanger();
+                    blueBall[2] *= -1;
                 }
 
                 if (redBall[0] >= getWidth()) {
-                    redBall[2] *= generateChanger();
+                    redBall[2] *= -1;
                 }
                 if (greenBall[0] >= getWidth()) {
-                    greenBall[2] *= generateChanger();
+                    greenBall[2] *= -1;
                 }
                 if (blueBall[0] >= getWidth()) {
-                    blueBall[2] *= generateChanger();
+                    blueBall[2] *= -1;
                 }
 
                 if (redBall[1] >= getHeight()) {
@@ -100,22 +92,19 @@ public class BrickBreaker extends JFrame {
 
                 if (redBall[0] >= plane && redBall[0] - 20 <= plane + 150) {
                     if (redBall[1] + 20 >= 700 && redBall[1] - 20 <= 720) {
-                        redBall[2] *= generateChanger();
-                        redBall[3] *= generateChanger();
+                        redBall[3] *= -1;
                     }
                 }
 
                 if (greenBall[0] >= plane && greenBall[0] - 20 <= plane + 150) {
                     if (greenBall[1] + 20 >= 700 && greenBall[1] - 20 <= 720) {
-                        greenBall[2] *= generateChanger();
-                        greenBall[3] *= generateChanger();
+                        greenBall[3] *= -1;
                     }
                 }
 
                 if (blueBall[0] >= plane && blueBall[0] - 20 <= plane + 150) {
                     if (blueBall[1] + 20 >= 700 && blueBall[1] - 20 <= 720) {
-                        blueBall[2] *= generateChanger();
-                        blueBall[3] *= generateChanger();
+                        blueBall[3] *= -1;
                     }
                 }
 
@@ -126,30 +115,36 @@ public class BrickBreaker extends JFrame {
                     g.fillRect(brick.p.x, brick.p.y, 40, 40);
 
                     if (brick.color == Color.RED) {
-                        if (brick.p.x <= redBall[0] && brick.p.x + 40 >= redBall[0]) {
-                            if (brick.p.y <= redBall[1] && brick.p.y + 40 >= redBall[1]) {
+                        if (brick.p.x <= redBall[0]+20 && brick.p.x + 40 >= redBall[0]) {
+                            if (brick.p.y <= redBall[1]+20 && brick.p.y + 40 >= redBall[1]) {
                                 forRemoval.add(brick);
-                                redBall[2] *= generateChanger();
+                                score++;
+                                redBall[2] *= -1;
+                                if (redBall[2] <= 4) redBall[2] = 1;
                                 if (redBall[2] <= 3) redBall[2] = 1;
                             }
                         }
                     }
 
                     if (brick.color == Color.GREEN) {
-                        if (brick.p.x <= greenBall[0] && brick.p.x + 40 >= greenBall[0]) {
-                            if (brick.p.y <= greenBall[1] && brick.p.y + 40 >= greenBall[1]) {
+                        if (brick.p.x <= greenBall[0]+20 && brick.p.x + 40 >= greenBall[0]) {
+                            if (brick.p.y <= greenBall[1]+20 && brick.p.y + 40 >= greenBall[1]) {
                                 forRemoval.add(brick);
-                                greenBall[2] *= generateChanger();
+                                score++;
+                                greenBall[2] *= -1;
+                                if (greenBall[2] <= 4) greenBall[2] = 1;
                                 if (greenBall[2] <= 3) greenBall[2] = 1;
                             }
                         }
                     }
 
                     if (brick.color == Color.BLUE) {
-                        if (brick.p.x <= blueBall[0] && brick.p.x + 40 >= blueBall[0]) {
-                            if (brick.p.y <= blueBall[1] && brick.p.y + 40 >= blueBall[1]) {
+                        if (brick.p.x <= blueBall[0]+20 && brick.p.x + 40 >= blueBall[0]) {
+                            if (brick.p.y <= blueBall[1]+20 && brick.p.y + 40 >= blueBall[1]) {
                                 forRemoval.add(brick);
-                                blueBall[2] *= generateChanger();
+                                score++;
+                                blueBall[2] *= -1;
+                                if (blueBall[2] <= 4) blueBall[2] = 1;
                                 if (blueBall[2] <= 3) blueBall[2] = 1;
                             }
                         }
@@ -159,19 +154,19 @@ public class BrickBreaker extends JFrame {
 
                 //drawing the red ball
                 g.setColor(Color.RED);
-                g.fillArc(redBall[0], redBall[1], 20, 20, 0, 360);
+                g.fillRect(redBall[0], redBall[1], 20, 20);
                 redBall[0] += redBall[2];
                 redBall[1] += redBall[3];
 
                 //drawing the green ball
                 g.setColor(Color.GREEN);
-                g.fillArc(greenBall[0], greenBall[1], 20, 20, 0, 360);
+                g.fillRect(greenBall[0], greenBall[1], 20, 20);
                 greenBall[0] += greenBall[2];
                 greenBall[1] += greenBall[3];
 
                 //drawing the blue ball
                 g.setColor(Color.BLUE);
-                g.fillArc(blueBall[0], blueBall[1], 20, 20, 0, 360);
+                g.fillRect(blueBall[0], blueBall[1], 20, 20);
                 blueBall[0] += blueBall[2];
                 blueBall[1] += blueBall[3];
 
@@ -182,6 +177,7 @@ public class BrickBreaker extends JFrame {
                 g.setColor(Color.WHITE);
                 g.setFont(new Font("Bold", Font.BOLD, 30));
                 g.drawString("Time: " + ((System.currentTimeMillis() - start) / 1000) + " Seconds", 50, 50);
+                g.drawString("Score: " + score, 50, 100);
             }
         }
     }
@@ -204,7 +200,7 @@ public class BrickBreaker extends JFrame {
         setFocusable(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setTitle("Brick Breaker -Paused");
+        setTitle("Brick Breaker");
         add(screen);
         Thread gameLoop;
         gameLoop = new Thread() {
@@ -214,7 +210,7 @@ public class BrickBreaker extends JFrame {
                 int frames = 0;
                 while (active) {
                     if (!pause) {
-                        while (frames <= 50) {
+                        while (frames <= 30) {
                             if (isFocused()) screen.repaint();
                             try {
                                 wait(20);
@@ -238,11 +234,6 @@ public class BrickBreaker extends JFrame {
         if (e.getKeyChar() == 'a') plane -= 10;
         if (e.getKeyChar() == ' ') {
             pause = !pause;
-            if (!pause) {
-                setTitle("Brick Breaker -Paused");
-            } else {
-                setTitle("Brick Breaker");
-            }
         }
     }
 }
